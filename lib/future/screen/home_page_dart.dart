0d4862c9b912fa52dbf2c8bbec0122cd70c9a7a8
Jamal Uddin <jamaluddin586755@gmail.com>
2025-01-07 17:30:27 +0600

    git add item

---
+++ lib/future/screen/home_page_dart.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../core/controller/controller.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final CounterController controller = Get.put(CounterController());
+
+    // controller.loadCounter();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('GetX Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(() => Text(
+              "${controller.counter.value}",
+            )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
